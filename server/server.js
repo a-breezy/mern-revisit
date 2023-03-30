9ed8bcde2213b43f037e828171c5165b9a23549d
a.breezy <ambrose.wilkins@gmail.com>
2023-03-30 13:35:19 -0400

    added server changes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+// create apollo server and pass schema data
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// create async apollo server with graphql schema
+const startApolloServer = async(typeDefs, resolvers) => {
+  await server.start();
+  // integrate apollo server with express middleware
+  server.applyMiddleware({app})
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+}
+
+// call function to start server
+startApolloServer(typeDefs, resolvers)
